15ba19f3489ee28a0f05703bf8d77eeb4b6ed031
Zahra Ghavasieh <judgyknowitall@hotmail.com>
2021-10-16 12:27:48 -0600

    Working on new Post Analysis Tab

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,12 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using System.Security.Principal;
 using System.Windows;
 using System.Windows.Controls;
-using VisualGaitLab.GaitAnalysis;
 using VisualGaitLab.OtherWindows;
 using VisualGaitLab.SupportingClasses;
 
@@ -184,137 +181,6 @@ namespace VisualGaitLab
 
 
 
-
-
-
-
-
-
-
-        // MARK: Gait Methods
-
-        private void PrepareGaitTab() { //set up the last (gait) tab = grab the analyzed videos from the analysis tab and make them show up here + set up logic for drag and drop to the other list box (combined gait export feature)
-            GaitVideos = new List<AnalysisVideo>();
-            GaitCombineListBox.AllowDrop = true;
-            GaitCombinedVideos = new List<AnalysisVideo>();
-            CombineResultsButton.IsEnabled = false;
-            foreach (AnalysisVideo vid in CurrentProject.AnalysisVideos) { //the combo box will contain all analyzed videos from the "Analyze" tab
-                if (vid.IsAnalyzed) {
-                    string gaitStateFolder = vid.Path.Substring(0, vid.Path.LastIndexOf("\\")) + "\\gaitsavedstate";
-                    if (Directory.Exists(gaitStateFolder) && File.Exists(gaitStateFolder + "\\metrics.txt")) {
-                        vid.GaitAnalyzedImageName = "Images/check.png";
-                        vid.IsGaitAnalyzed = true;
-                    }
-                    else {
-                        vid.GaitAnalyzedImageName = "Images/cross.png";
-                        vid.IsGaitAnalyzed = false;
-                    }
-                    GaitVideos.Add(vid);
-                }
-            }
-
-            if (GaitVideos.Count > 0) { //if there are analyzed videos allow access to the "Gait" tab
-                GaitListBox.ItemsSource = null;
-                GaitListBox.ItemsSource = GaitVideos;
-                GaitTab.IsEnabled = true;
-            }
-            else GaitTab.IsEnabled = false; //if there are none disable it
-        }
-
-        private void GaitListBox_SelectionChanged(object sender, SelectionChangedEventArgs e) { //change button description depending on whether gait has been analyzed on a particular video
-            AnalysisVideo selectedVid = (AnalysisVideo)GaitListBox.SelectedItem;
-            if (selectedVid != null) {
-                AnalyzeGaitButton.IsEnabled = true;
-                if (selectedVid.IsGaitAnalyzed) {
-                    AnalyzeGaitButton.Content = "View/Edit";
-                }
-                else {
-                    AnalyzeGaitButton.Content = "Analyze Gait";
-                }
-            }
-            else AnalyzeGaitButton.IsEnabled = false;
-        }
-
-        private void AnalyzeGaitButton_Click(object sender, RoutedEventArgs e) {
-            AnalysisVideo vid = (AnalysisVideo)GaitListBox.SelectedItem; //get the selected video
-            if (vid != null) {
-                string gaitVideoPath = vid.Path; //extract necessary info from the video
-                string gaitVideoName = gaitVideoPath.Substring(gaitVideoPath.LastIndexOf("\\") + 1, gaitVideoPath.LastIndexOf(".") - gaitVideoPath.LastIndexOf("\\"));
-                string gaitTempPath = gaitVideoPath.Substring(0, gaitVideoPath.LastIndexOf("\\")) + "\\temp-" + gaitVideoName;
-                var files = Directory.EnumerateFiles(gaitTempPath);
-                var file = ""; //might crash
-                foreach(var currentImg in files) {
-                    if (currentImg.Contains(".png")) {
-                        file = currentImg;
-                        break;
-                    }
-                }
-                BarInteraction();
-
-                // Check if Gait data is saved from before
-                string stateFolder = gaitVideoPath.Substring(0, gaitVideoPath.LastIndexOf("\\")) + "\\gaitsavedstate";
-                if (Directory.Exists(stateFolder) && File.Exists(stateFolder + "\\inputParams.txt")) {
-                    GaitWindow gaitWindow = new GaitWindow(gaitVideoPath, gaitVideoName, gaitTempPath);
-                    if (gaitWindow.ShowDialog() == true) {
-                        SyncUI();
-                    }
-                    EnableInteraction();
-                }
-                else //input params not saved, ask the user for'em
-                {
-                    MeasureWindow window = new MeasureWindow(file, stateFolder); //spawn a window through which the user will give us the treadmill speed and a real world reference (for distance measurements)
-                    if (window.ShowDialog() == true) {
-                        double realWorldMultiplier = window.getSinglePixelSize();
-                        float treadmillSpeed = float.Parse(window.TreadmillSpeedTextBox.Text);
-                        bool isFreeRun = false;
-                        if ((bool)window.AnalysisTypeRadioFreeWalking.IsChecked) isFreeRun = true;
-
-                        GaitWindow gaitWindow = new GaitWindow(realWorldMultiplier, treadmillSpeed, gaitVideoPath, gaitVideoName, gaitTempPath, isFreeRun);
-                        if (gaitWindow.ShowDialog() == true) {
-                            SyncUI();
-                        }
-                        EnableInteraction();
-                    }
-                    else EnableInteraction();
-                }
-            }
-        }
-
-        private void CombineResultsButton_Click(object sender, RoutedEventArgs e) {
-            ExportCombinedGait();
-        }
-
-        private void DragStarted(object sender, System.Windows.Input.MouseButtonEventArgs e) { //started dragging an item in the Gait Listbox
-            DragSource = (ListBox)sender;
-            object data = DragSource.SelectedItem;
-
-            if (data != null) {
-                DragDrop.DoDragDrop(DragSource, data, DragDropEffects.Move);
-            }
-        }
-
-        private void DragDropped(object sender, DragEventArgs e) { //dropped a dragged item above the Gait "combined export" Listbox
-            DragTarget = (ListBox)sender;
-            AnalysisVideo draggedData = (AnalysisVideo)DragSource.SelectedItem;
-            if (!GaitCombinedVideos.Contains(draggedData) && draggedData.IsGaitAnalyzed) {
-                GaitCombinedVideos.Add(draggedData);
-                GaitCombineListBox.ItemsSource = null;
-                GaitCombineListBox.ItemsSource = GaitCombinedVideos;
-                DragNDropLabel.Visibility = Visibility.Collapsed;
-                if (GaitCombinedVideos.Count > 1) CombineResultsButton.IsEnabled = true;
-            }
-        }
-
-        private void EmptyButton_Click(object sender, RoutedEventArgs e) {
-            ClearCombinedGait();
-        }
-
-
-
-
-
-
-
         private void DebugConsoleClicked(object sender, RoutedEventArgs e) {
             if(MessageBox.Show("Debug Console displays the command line each time VGL is interfacing with DeepLabCut. Enable this if you're experiencing problems with creating a project, labelling, training, or analysis. If you encounter a bug, enable this, and email rfiker@ucalgary.ca with a screenshot of the console. Do you want to show the console?"
                 ,"Enable Debug Console?", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes){
@@ -323,6 +189,11 @@ namespace VisualGaitLab
             else {
                 UpdateSettings(false);
             }
+        }
+
+        private void PAVidListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
---
+++ MainWindow/GaitTabMethods.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using VisualGaitLab.SupportingClasses;
+using VisualGaitLab.GaitAnalysis;
+using System.IO;
+using System.Windows.Controls;
+
+namespace VisualGaitLab
+{
+    public partial class MainWindow : Window
+    {
+        private void PrepareGaitTab()
+        { //set up the last (gait) tab = grab the analyzed videos from the analysis tab and make them show up here + set up logic for drag and drop to the other list box (combined gait export feature)
+            GaitVideos = new List<AnalysisVideo>();
+            GaitCombineListBox.AllowDrop = true;
+            GaitCombinedVideos = new List<AnalysisVideo>();
+            CombineResultsButton.IsEnabled = false;
+            foreach (AnalysisVideo vid in CurrentProject.AnalysisVideos)
+            { //the combo box will contain all analyzed videos from the "Analyze" tab
+                if (vid.IsAnalyzed)
+                {
+                    string gaitStateFolder = vid.Path.Substring(0, vid.Path.LastIndexOf("\\")) + "\\gaitsavedstate";
+                    if (Directory.Exists(gaitStateFolder) && File.Exists(gaitStateFolder + "\\metrics.txt"))
+                    {
+                        vid.GaitAnalyzedImageName = "Images/check.png";
+                        vid.IsGaitAnalyzed = true;
+                    }
+                    else
+                    {
+                        vid.GaitAnalyzedImageName = "Images/cross.png";
+                        vid.IsGaitAnalyzed = false;
+                    }
+                    GaitVideos.Add(vid);
+                }
+            }
+
+            if (GaitVideos.Count > 0)
+            { //if there are analyzed videos allow access to the "Gait" tab
+                GaitListBox.ItemsSource = null;
+                GaitListBox.ItemsSource = GaitVideos;
+                GaitTab.IsEnabled = true;
+            }
+            else GaitTab.IsEnabled = false; //if there are none disable it
+        }
+
+        private void GaitListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        { //change button description depending on whether gait has been analyzed on a particular video
+            AnalysisVideo selectedVid = (AnalysisVideo)GaitListBox.SelectedItem;
+            if (selectedVid != null)
+            {
+                AnalyzeGaitButton.IsEnabled = true;
+                if (selectedVid.IsGaitAnalyzed)
+                {
+                    AnalyzeGaitButton.Content = "View/Edit";
+                }
+                else
+                {
+                    AnalyzeGaitButton.Content = "Analyze Gait";
+                }
+            }
+            else AnalyzeGaitButton.IsEnabled = false;
+        }
+
+        private void AnalyzeGaitButton_Click(object sender, RoutedEventArgs e)
+        {
+            AnalysisVideo vid = (AnalysisVideo)GaitListBox.SelectedItem; //get the selected video
+            if (vid != null)
+            {
+                string gaitVideoPath = vid.Path; //extract necessary info from the video
+                string gaitVideoName = gaitVideoPath.Substring(gaitVideoPath.LastIndexOf("\\") + 1, gaitVideoPath.LastIndexOf(".") - gaitVideoPath.LastIndexOf("\\"));
+                string gaitTempPath = gaitVideoPath.Substring(0, gaitVideoPath.LastIndexOf("\\")) + "\\temp-" + gaitVideoName;
+                var files = Directory.EnumerateFiles(gaitTempPath);
+                var file = ""; //might crash
+                foreach (var currentImg in files)
+                {
+                    if (currentImg.Contains(".png"))
+                    {
+                        file = currentImg;
+                        break;
+                    }
+                }
+                BarInteraction();
+
+                // Check if Gait data is saved from before
+                string stateFolder = gaitVideoPath.Substring(0, gaitVideoPath.LastIndexOf("\\")) + "\\gaitsavedstate";
+                if (Directory.Exists(stateFolder) && File.Exists(stateFolder + "\\inputParams.txt"))
+                {
+                    GaitWindow gaitWindow = new GaitWindow(gaitVideoPath, gaitVideoName, gaitTempPath);
+                    if (gaitWindow.ShowDialog() == true)
+                    {
+                        SyncUI();
+                    }
+                    EnableInteraction();
+                }
+                else //input params not saved, ask the user for'em
+                {
+                    MeasureWindow window = new MeasureWindow(file, stateFolder); //spawn a window through which the user will give us the treadmill speed and a real world reference (for distance measurements)
+                    if (window.ShowDialog() == true)
+                    {
+                        double realWorldMultiplier = window.getSinglePixelSize();
+                        float treadmillSpeed = float.Parse(window.TreadmillSpeedTextBox.Text);
+                        bool isFreeRun = false;
+                        if ((bool)window.AnalysisTypeRadioFreeWalking.IsChecked) isFreeRun = true;
+
+                        GaitWindow gaitWindow = new GaitWindow(realWorldMultiplier, treadmillSpeed, gaitVideoPath, gaitVideoName, gaitTempPath, isFreeRun);
+                        if (gaitWindow.ShowDialog() == true)
+                        {
+                            SyncUI();
+                        }
+                        EnableInteraction();
+                    }
+                    else EnableInteraction();
+                }
+            }
+        }
+
+        private void CombineResultsButton_Click(object sender, RoutedEventArgs e)
+        {
+            ExportCombinedGait();
+        }
+
+        private void DragStarted(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        { //started dragging an item in the Gait Listbox
+            DragSource = sender as ListBox;
+            object data = DragSource.SelectedItem;
+
+            if (data != null)
+            {
+                DragDrop.DoDragDrop(DragSource, data, DragDropEffects.Move);
+            }
+        }
+
+        private void DragDropped(object sender, DragEventArgs e)
+        { //dropped a dragged item above the Gait "combined export" Listbox
+            DragTarget = (ListBox)sender;
+            AnalysisVideo draggedData = (AnalysisVideo)DragSource.SelectedItem;
+            if (!GaitCombinedVideos.Contains(draggedData) && draggedData.IsGaitAnalyzed)
+            {
+                GaitCombinedVideos.Add(draggedData);
+                GaitCombineListBox.ItemsSource = null;
+                GaitCombineListBox.ItemsSource = GaitCombinedVideos;
+                DragNDropLabel.Visibility = Visibility.Collapsed;
+                if (GaitCombinedVideos.Count > 1) CombineResultsButton.IsEnabled = true;
+            }
+        }
+
+        private void EmptyButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearCombinedGait();
+        }
+    }
+}
